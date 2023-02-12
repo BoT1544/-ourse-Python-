ff09e75c7187775303b95a1e0c495c6439585357
BoT143 <you@example.com>
2023-02-12 07:54:04 +0300

    Added Folder: .folder

---
+++ .folder/lectures.py
@@ -0,0 +1,11 @@
+# print('Введите первое число: ')
+# a = int(input())
+# b = int(input('input second number:'))
+
+# print(a, ' + ', b, ' = ', a + b)
+
+a = 5.8999952
+b = 6.62626
+print (round(a*b, 3))
+
+
