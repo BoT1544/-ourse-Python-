4e29e1de70e711fb201f03bbcff4f5be8e3dd4b0
BoT143 <you@example.com>
2023-03-02 09:44:31 +0300

    Homework 6 DONE ! ! ! )

---
+++ Lesson 6/Homework 6.py
@@ -0,0 +1,30 @@
+"""
+Заполните массив элементами арифметической прогрессии. Её первый элемент, разность и количество элементов нужно ввести с клавиатуры. Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+Каждое число вводится с новой строки.
+"""
+
+# first_number = int(input('Input number for start list: '))
+# step = int(input('Input list step: '))
+# size_lst = int(input('Input list size: '))
+# lst = []
+# lst.append(first_number)
+# for i in range(1, size_lst):
+#     first_number += step
+#     lst.append(first_number)
+# print(lst)
+
+
+"""
+Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
+"""
+
+# import random
+# my_list = [random.randint(-5,20) for _ in range(int(input('Input size list: ')))]
+# print(my_list)
+# my_list1 = []
+# range1 = 5
+# range2 = 15
+# for i in range(len(my_list)):
+#     if my_list[i] in range(5,15):
+#         my_list1.append(i)
+# print(my_list1)
