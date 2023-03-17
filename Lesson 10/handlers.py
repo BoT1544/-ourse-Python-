9aa669a36a835d2433f38ad307fdabac4ba6a2d2
BoT143 <you@example.com>
2023-03-17 10:44:36 +0300

    Homework 9 DONE ! ! ! )

--- Lesson 10/handlers.py
+++ Lesson 10/handlers.py
@@ -6,30 +6,63 @@ import config
 @dp.message_handler(commands=['start', 'начать'])
 async def mes_start(message: Message):
     await message.answer(text=f'{message.from_user.first_name}, привет !\n'
-                        f'Сегодня мы с тобой поиграем в интересную игру')
+                        f'Сегодня мы с тобой поиграем в интересную игру\n'
+                        'Для начала игры введи команду и через пробел укажи количество конфет\n'
+                        'Пример ввода "/new 100"')
 
 
 @dp.message_handler(commands=['new'])
-async def mes_new_game(message: Message):
-    name = message.from_user.first_name
-    for game in config.games:
-        if message.from_user.id == game:
-            await message.answer(f'{name} ты уже есть в игре, иди играй')
-            break
-    else:
-        config.total = 150
-        await message.answer(text=f'И так, на столе {config.total} конфет. Кидаем жребий кто берёт первым')
-        coin = random.randint(0, 1)
-        
-
-        config.games[message.from_user.id] = 150
-        if coin:
-            await message.answer(text=f'{message.from_user.first_name}, поздравляю выпал орёл. Ты ходишь первым'
-                                    f'бери от 1 до 28 конфет')
+async def mes_new_game(message: Message,):
+    if ' ' in message.text:
+        if message.text.split(' ')[1].isdigit():
+            candy = int(message.text.split(' ')[1])
+            name = message.from_user.first_name
+            for game in config.games:
+                if message.from_user.id == game:
+                    await message.answer(f'{name} ты уже есть в игре, иди играй')
+                    break
+            else:
+                config.total = candy
+                await message.answer(text=f'И так, на столе {config.total} конфет. Кидаем жребий кто берёт первым')
+                config.games[message.from_user.id] = candy
+                coin = random.randint(0, 1)
+                if coin:
+                    await message.answer(text=f'{message.from_user.first_name}, поздравляю выпал орёл. Ты ходишь первым'
+                                            f'бери от 1 до 28 конфет')
+                else:
+                    await message.answer(text=f'{message.from_user.first_name}, не растраивайся первый ход делает бот')
+                    await bot_turn(message)
         else:
-            await message.answer(text=f'{message.from_user.first_name}, не растраивайся первый ход делает бот')
-            await bot_turn(message)
+            await message.reply(f'Вы ввели {message.text}, а нужно правильно команду "/new"\n'
+                                'После ввести количество конфет через пробел\n'
+                                'Пример ввода: "/new 100')            
+    else:
+        await message.reply(f'Вы ввели {message.text}, нужно были разделить команду и цифры пробелом\n'
+                                'Пример ввода: "/new 100')
+            
 
+
+
+"""рабочая версия"""
+# @dp.message_handler(commands=['new'])
+# async def mes_new_game(message: Message):
+#     name = message.from_user.first_name
+#     for game in config.games:
+#         if message.from_user.id == game:
+#             await message.answer(f'{name} ты уже есть в игре, иди играй')
+#             break
+#     else:
+#         config.total = 150
+#         await message.answer(text=f'И так, на столе {config.total} конфет. Кидаем жребий кто берёт первым')
+#         config.games[message.from_user.id] = 150
+#         coin = random.randint(0, 1)
+#         if coin:
+#             await message.answer(text=f'{message.from_user.first_name}, поздравляю выпал орёл. Ты ходишь первым'
+#                                     f'бери от 1 до 28 конфет')
+#         else:
+#             await message.answer(text=f'{message.from_user.first_name}, не растраивайся первый ход делает бот')
+#             await bot_turn(message)
+""""""
 
 @dp.message_handler()
 async def all_catch(message: Message):
@@ -40,7 +73,11 @@ async def all_catch(message: Message):
             await message.answer(text=f'Ах ты, хитрый {message.from_user.first_name} !'
                                  f'Конфет надо взять хотябы 1, но не больше 28. Попробуй ещё раз ')
     else:
-        await message.answer(text='введи цифрами количество конфет от 1 до 28')
+        if '/new' in message.text:
+            await message.reply(f'У вас введено {message.text}, а нужно вводить вот так: "/new 100"\n'
+                                'цифру можно любую больше 100, а то будет не интересно ! )')
+        else:
+            await message.answer(text='введи цифрами количество конфет от 1 до 28')
 
 async def player_turn(message: Message):
     take_amount = int(message.text)
@@ -53,6 +90,8 @@ async def player_turn(message: Message):
     await message.answer(text=f'Торжественно передаём ход боту !') 
     await bot_turn(message)
 
+
+"""рабочая версия !"""
 async def bot_turn(message: Message):
         take_amount = 0
         current_total = config.games.get(message.from_user.id)
@@ -68,7 +107,32 @@ async def bot_turn(message: Message):
         if await check_victory(message, 'бот'):
             return
         await message.answer(text=f'{name} теперь твоё черёд ! Бери конфеты') 
-        
+
+""""""
+
+# async def bot_turn(message: Message):
+#     await message.answer('Давайте выберем сложность !\n'
+#                          'Введите цифру 0 если хотите нормальную сложность\n'
+#                          'Введите цифру 1 если хотите высокую сложность')
+#     if message.text == '1':
+#         take_amount = 0
+#         current_total = config.games.get(message.from_user.id)
+#         if current_total <= 28:
+#             take_amount = current_total
+#         else:
+#             take_amount = current_total % 29 if current_total % 29 != 0 else 1
+#     else:
+#         take_amount = random.randint(1, 28)
+#     config.games[message.from_user.id] = config.games.get(message.from_user.id) - take_amount
+#     name = message.from_user.first_name
+#     await message.answer(text=f'бот взял {take_amount} конфет и на столе осталось'
+#                          f'{config.games.get(message.from_user.id)}\n')
+#     if await check_victory(message, 'бот'):
+#         return
+#     await message.answer(text=f'{name} теперь твоё черёд ! Бери конфеты')
+
+
+
 async def check_victory(message: Message, name: str):
     if config.games.get(message.from_user.id) <= 0:
         await message.answer(text=f'Победил {name} ! "Это была славная игра ! )')
@@ -77,10 +141,3 @@ async def check_victory(message: Message, name: str):
     else:
         return False
 
-
-
-
-
-
-    # await message.answer(text=f'{message.from_user.first_name}, привет !\n'
-    #                     f'Сегодня мы с тобой поиграем в интересную игру')
--- Lesson 9/Homework 9.py
+++ Lesson 9/Homework 9.py
@@ -11,11 +11,23 @@ async def on_start(_):
 
 @dp.message_handler(commands=['start'])
 async def com_start(message: Message):
-    await message.reply('Здравствуйте, я умею складывать и вычитать ! )')
+    name = message.from_user.first_name
+    await message.reply(f'Привет {name}, я Кальк_Ботяо ! )\nЕсли напишешь "/help", то раскажу что умею ! )')
+
+@dp.message_handler(commands=['help'])
+async def com_start(message: Message):
+    name = message.from_user.first_name
+    await message.answer(f'{name} ну смотри ниже я приведу примеры:\n'
+                         '1) Складываю вот так: "1+1"\n'
+                         '2) Вычитаю вот так: "2-1"\n'
+                         '3) Умножаю вот так: "2*2"\n'
+                         '4) Делю вот так: "2/2"\n'
+                         'Пока что это всё, но я учусь ! )')
 
 
 @dp.message_handler()
 async def com_start(message: Message):
+    name = message.from_user.first_name
     string = str(message.text)
     for i in string:
         if i == '+':
@@ -25,21 +37,55 @@ async def com_start(message: Message):
                     num_2 = int(string.split('+')[1])
                     sum = num_1 + num_2
                     print(sum)
-                    await message.answer(f'Сумма чисел {num_1} и {num_2} равняется {sum}')
+                    await message.answer(f'Пуп-Бип, у меня получилось ! ! ! )\nСумма чисел {num_1} и {num_2} равняется {sum}')
                     break
-                await message.reply(f'{message.from_user.first_name} у вас {message.text}, а должно быть без букв примерно так: "1+1"') 
+                await message.reply(f'Буп-Бип, {name} у вас {message.text}, а должно быть без букв и пробелов примерно так: "1+1"') 
                 break
-            await message.reply(f'{message.from_user.first_name} у вас {message.text}, а должно быть без букв примерно так: "1+1"')
+            await message.reply(f'Буп-Бип, {name} у вас {message.text}, а должно быть без букви пробелов примерно так: "1+1"')
             break   
         elif i == '-':
-            num_1 = int(string.split('-')[0])
-            num_2 = int(string.split('-')[1])
-            sum = num_1 - num_2
-            print(sum)
-            await message.answer(f'Разность чисел {num_1} и {num_2} равняется {sum}')
-            break   
+            if string.split('-')[0].isdigit():
+                if string.split('-')[1].isdigit():
+                    num_1 = int(string.split('-')[0])
+                    num_2 = int(string.split('-')[1])
+                    sum = num_1 - num_2
+                    print(sum)
+                    await message.answer(f'Бип-пип, у меня получилось ! ! ! )\nРазность чисел {num_1} и {num_2} равняется {sum}')
+                    break
+                await message.reply(f'Буп-Бип, {name} у вас {message.text}, а должно быть без букв и пробелов примерно так: "2-1"') 
+                break
+            await message.reply(f'Буп-Бип, {name} у вас {message.text}, а должно быть без букв и пробелов примерно так: "2-1"')
+            break
+        elif i == '*':
+            if string.split('*')[0].isdigit():
+                if string.split('*')[1].isdigit():
+                    num_1 = int(string.split('*')[0])
+                    num_2 = int(string.split('*')[1])
+                    sum = num_1 * num_2
+                    print(sum)
+                    await message.answer(f'Буп-пуп, у меня получилось ! ! ! )\n{num_1} умножить на {num_2} равняется {sum}')
+                    break
+                await message.reply(f'Буп-Бип, {name} у вас {message.text}, а должно быть без букв и пробелов примерно так: "2*2"') 
+                break
+            await message.reply(f'Буп-Бип, {name} у вас {message.text}, а должно быть без букв и пробелов примерно так: "2*2"')
+            break
+        elif i == '/':
+            if string.split('/')[0].isdigit():
+                if string.split('/')[1].isdigit():
+                    num_1 = int(string.split('/')[0])
+                    num_2 = int(string.split('/')[1])
+                    sum = num_1 / num_2
+                    sum = round(sum, 3)
+                    print(sum)
+                    await message.answer(f'Бип-Пуп, у меня получилось ! ! ! )\n{num_1} разделить на {num_2} равняется {sum}')
+                    break
+                await message.reply(f'Буп-Бип, {name} у вас {message.text}, а должно быть без букв и пробелов примерно так: "2/2"') 
+                break
+            await message.reply(f'Буп-Бип, {name} у вас {message.text}, а должно быть без букв и пробелов примерно так: "2/2"')
+            break
     if i == string[-1]:
-        await message.answer('Неверный ввод. Пример ввода: "1+1" или "2-1')
+        await message.reply('Бип-Буп, моя не знать что с этим делать.\n'
+                            'Поробуй ввести вот так:\n"1+1" или "2-1" или "2*2" или "2/2"')
 
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_start)
